90651d4e9bc48abe48d7e46ddefa1a0d487123a2
amaazing <maazali7070@gmail.com>
2025-02-17 16:10:22 -0500

    Integrate color.py and colouring.py

--- src/utils/colouring.py
+++ src/utils/colouring.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import pandas as pd
 import os
+from skimage.exposure import match_histograms
+
 
 # RGB values for Canadian and Japanese lean color standards
 canadian_rgb = np.array([
@@ -42,6 +44,60 @@ japanese_rgb_standard = np.array([
     (191, 124, 75), # J1
 ], dtype=np.float32)
 
+#######################################
+#####Standardization Functions#########
+#######################################
+def white_balance(image, option):
+    '''
+    Balance the white in an image
+    Helps reduce lighting impact.
+    Simple WB: Result is closer to the original, but increased variance.
+    Learning WB: Result has less variance.
+    '''
+    if option == "SimpleWB":
+        result = cv2.xphoto.createSimpleWB().balanceWhite(image)
+    if option == "LearnWB":
+        result = cv2.xphoto.createLearningBasedWB().balanceWhite(image)
+    return result
+
+def LAB_check(reference_image, image):
+    lab_ref = cv2.cvtColor(reference_image, cv2.COLOR_BGR2LAB)
+    lab_current = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l_ref, a_ref, b_ref = cv2.split(lab_ref)
+    l_cur, a_cur, b_cur = cv2.split(lab_current)
+    if (np.mean(l_cur)/np.mean(l_ref) < 0.99) or (np.mean(l_cur)/np.mean(l_ref) > 1.01) \
+    or (np.mean(a_cur)/np.mean(a_ref) < 0.99) or (np.mean(a_cur)/np.mean(a_ref) > 1.01) \
+    or (np.mean(b_cur)/np.mean(b_ref) < 0.99) or (np.mean(b_cur)/np.mean(b_ref) > 1.01):
+        #print("Outside margin of error, add to correction list")
+        standard_img  = reference_standardize(image, reference_image)
+        return standard_img
+    else:
+        #print("Within margin of error, no need to standardize")
+        return image
+
+def reference_standardize(image, reference_image):
+    '''
+    Takes in a list of images, and a reference.
+    Standardizes the list of images to the reference
+    by matching histograms.
+    Returns standardized image.
+    '''
+    standard_img = match_histograms(image, reference_image, channel_axis=-1)
+    #standard_img = cv2.medianBlur(standard_img, 3) Used just to approximate Category cutoffs
+    return standard_img
+
+def execute_color_standardization(image):
+    reference_image = cv2.imread('data/reference_images/2704_LdLeanColor.JPG')
+    #reference_image = white_balance(reference_image, "SimpleWB")
+
+    #balance = white_balance(image, "SimpleWB")
+    standardized_image = LAB_check(reference_image, image)
+    return standardized_image
+
+
+############################
+#####ANALYSIS FUNCTIONS#####
+############################
 def classify_rgb_vectorized(image, standards, lean_mask):
     """Vectorized classification of RGB pixels using Euclidean distance."""
     h, w, _ = image.shape
@@ -90,11 +146,15 @@ def colour_grading(image, muscle_mask, marbling_mask, output_dir, image_id):
     canadian_lut_image = apply_lut(canadian_classified, list(range(7)), canadian_rgb, lean_mask)
     japanese_lut_image = apply_lut(japanese_classified, list(range(6)), japanese_rgb, lean_mask)
     
+    # Creates a standardization for the image
+    standard_img = execute_color_standardization(image)
+
     # Save results
     base_output_dir = os.path.join(output_dir, image_id)
     os.makedirs(base_output_dir, exist_ok=True)
     cv2.imwrite(os.path.join(base_output_dir, f"{image_id}_canadian_lut.png"), canadian_lut_image)
     cv2.imwrite(os.path.join(base_output_dir, f"{image_id}_japanese_lut.png"), japanese_lut_image)
+    cv2.imwrite(os.path.join(base_output_dir, f"{image_id}_STANDARDIZED.png"), standard_img)
 
     return canadian_classified, japanese_classified, lean_mask
 
