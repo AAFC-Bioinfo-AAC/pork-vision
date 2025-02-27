c3999e5a7b1e07f32e51ef73aececb39c4b2a336
amaazing <maazali7070@gmail.com>
2025-02-27 17:15:01 +0000

    Add slurm shell script

---
+++ porkvision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --job-name=porkvision
+#SBATCH --output=porkvision.out
+#SBATCH --cluster=gpsc8
+#SBATCH --partition=standard
+#SBATCH --account=aafc_aac
+#SBATCH --time=01:30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --comment="registry.maze.science.gc.ca/ssc-hpcs/generic-job:ubuntu22.04,tmpfs_size=2G"
+#SBATCH --mail-type=END,FAIL,BEGIN
+
+./src/main.py
