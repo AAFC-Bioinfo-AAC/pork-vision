7232e269f9597e29c8e9a289f3e1f9a28d74968f
amaazing <maazali7070@gmail.com>
2025-04-09 15:43:52 +0000

    Changed to 2 hours time for a full run

--- porkvision.sh
+++ porkvision.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=porkvision
 #SBATCH --output=porkvision.out
 #SBATCH --cluster=gpsc7
-#SBATCH --partition=standard
-#SBATCH --account=aafc_aac
+#SBATCH --partition=gpu_a100
+#SBATCH --account=aafc_aac__gpu_a100
 #SBATCH --time=02:00:00
-#SBATCH --mem=100GB
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --comment="registry.maze.science.gc.ca/ssc-hpcs/generic-job:ubuntu22.04,tmpfs_size=2G"
 
 conda activate porkvision-1.0.0
 python ./src/main.py
+
