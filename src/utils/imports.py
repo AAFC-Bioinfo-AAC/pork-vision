63f9298c47767ce33b7bc6f78193fcb6cb0e2e94
amaazing <maazali7070@gmail.com>
2025-04-14 17:46:27 +0000

    Move args function from main to here

--- src/utils/imports.py
+++ src/utils/imports.py
@@ -2,3 +2,13 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run PorkVision Inference and Analysis")
+    parser.add_argument("--model_path", type=str, default="src/models/Yolo_MuscleFat_Segment_98epoch.pt")
+    parser.add_argument("--color_model_path", type=str, default="src/models/color_100_last.pt")
+    parser.add_argument("--image_path", type=str, default="data/")
+    parser.add_argument("--output_path", type=str, default="output/")
+    return parser.parse_args()
