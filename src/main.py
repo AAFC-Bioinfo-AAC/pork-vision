1bc4ef9aa4299143ec06d365d99c0c257fb90b81
amaazing <maazali7070@gmail.com>
2025-04-17 21:31:56 +0000

    Add debug_info

--- src/main.py
+++ src/main.py
@@ -26,14 +26,22 @@ from utils.postprocess import (
     save_to_roi
 )
 
+def debug_info(debug_messages,image_id,args):
+    if args.debug == True:
+        os.makedirs(f"{args.output_path}/debug", exist_ok=True)
+        with open(f'{args.output_path}/debug/{image_id}_DEBUGINFO.txt', 'w') as file:
+            for string in debug_messages:
+                file.write(string+"\n")
+            file.close()
+
 def process_image(model, image_path, args, color_model):
     """Process an image: Run YOLO inference, extract measurements, and save annotated output."""
     try:
         outlier = None
         color_outlier = None
         image_id = extract_image_id(image_path)
-        if args.debug == True:
-            debug_messages = []
+        debug_messages = []
+        debug_messages.append(f"{image_id}:")
         print("\nProcessing:", image_id)
         
         # Step 1: YOLO Inference
@@ -49,7 +57,8 @@ def process_image(model, image_path, args, color_model):
         muscle_bbox, muscle_mask, fat_bbox, fat_mask = mask_selector(results)
         if muscle_bbox is None or fat_bbox is None:
             outlier = "Y"
-            print(f"ERROR {image_id}: Did not detect a muscle/fat bounding box.")
+            debug_messages.append(f"ERROR: Did not detect a muscle/fat bounding box.")
+            debug_info(debug_messages, image_id, args)
             return extract_image_id(image_path), 0, 0, 0, 0, 0, 0, 0, 0, outlier, color_outlier, image_path, 0
 
         muscle_binary_mask = convert_contours_to_image(muscle_mask, results.orig_shape)
@@ -63,7 +72,7 @@ def process_image(model, image_path, args, color_model):
         # Step 4: Conversion Factor Calculation
         conversion_factor, outlier = measure_ruler(rotated_image, image_id, outlier, args.minimal)
         if conversion_factor == None:
-            print(f"ERROR {image_id}: Conversion Factor calculation, using default.")
+            debug_messages.append(f"ERROR: Conversion Factor calculation, using default.")
             conversion_factor = 10/140 #mm/px
         # Step 6: Create Canadian Standard chart using A.I model.
         canadian_standards = create_coloring_standards(rotated_image, color_model, image_id, args.output_path+'/colouring', args.minimal)
@@ -82,34 +91,39 @@ def process_image(model, image_path, args, color_model):
         angle = get_muscle_rotation_angle(rotated_muscle_mask)
         if angle is None:
             outlier = "Y"
-            print(f"ERROR {image_id}: ANGLE is None")
+            debug_messages.append(f"ERROR: ANGLE is None")
+            debug_info(debug_messages, image_id, args)
             return extract_image_id(image_path), 0, 0, 0, marbling_percentage, canadian_classified_standard, lean_mask, conversion_factor, area_px, outlier, color_outlier, image_path, area_mm
 
         muscle_width_start, muscle_width_end = measure_longest_horizontal_segment(rotated_muscle_mask, angle)
         if muscle_width_start is None or muscle_width_end is None:
             outlier = "Y"
-            print(f"ERROR {image_id}: Muscle width is None")
+            debug_messages.append(f"ERROR: Muscle width is None")
+            debug_info(debug_messages, image_id, args)
             return extract_image_id(image_path), 0, 0, 0, marbling_percentage, canadian_classified_standard, lean_mask, conversion_factor, area_px, outlier, color_outlier, image_path, area_mm
         muscle_width = np.linalg.norm(np.array(muscle_width_start) - np.array(muscle_width_end))
 
         midline_position, midline_point = find_midline_using_fat_extremes(rotated_fat_mask)
         if midline_position is None:
             outlier = "Y"
-            print(f"ERROR {image_id}: Midline position is None")
+            debug_messages.append(f"ERROR: Midline position is None")
+            debug_info(debug_messages, image_id, args)
             return extract_image_id(image_path), muscle_width, 0, 0, marbling_percentage, canadian_classified_standard, lean_mask, conversion_factor, area_px, outlier, color_outlier, image_path, area_mm
 
 
-        muscle_depth_start, muscle_depth_end = measure_vertical_segment(rotated_muscle_mask, midline_position, angle, conversion_factor/10)
+        muscle_depth_start, muscle_depth_end = measure_vertical_segment(rotated_muscle_mask, midline_position, angle, (1/(conversion_factor)*10))
         if muscle_depth_start is None or muscle_depth_end is None:
             outlier = "Y"
-            print(f"ERROR {image_id}: Muscle Depth is None")
+            debug_messages.append(f"ERROR: Muscle Depth is None")
+            debug_info(debug_messages, image_id, args)
             return extract_image_id(image_path), muscle_width, 0, 0, marbling_percentage, canadian_classified_standard, lean_mask, conversion_factor, area_px, outlier, color_outlier, image_path, area_mm
         muscle_depth = np.linalg.norm(np.array(muscle_depth_start) - np.array(muscle_depth_end))
 
         fat_depth_start, fat_depth_end = extend_vertical_line_to_fat(rotated_fat_mask, (muscle_depth_start, muscle_depth_end))
         if fat_depth_start is None or fat_depth_end is None:
             outlier = "Y"
-            print(f"ERROR {image_id}: Fat depth is None")
+            debug_messages.append(f"ERROR: Fat depth is None")
+            debug_info(debug_messages, image_id, args)
             return extract_image_id(image_path), muscle_width, muscle_depth, 0, marbling_percentage, canadian_classified_standard, lean_mask, conversion_factor, area_px, outlier, color_outlier, image_path, area_mm
         fat_depth = np.linalg.norm(np.array(fat_depth_start) - np.array(fat_depth_end))
 
@@ -130,7 +144,8 @@ def process_image(model, image_path, args, color_model):
                 image_id=extract_image_id(image_path),
                 rois_folder=args.output_path+'/rois'
             )
-
+        
+        debug_info(debug_messages, image_id, args)
         return (
             image_id,
             int(round(muscle_width)),
@@ -149,7 +164,8 @@ def process_image(model, image_path, args, color_model):
             
 
     except Exception as e:
-        print(f"Error processing {image_id}: {e}")
+        debug_messages.append(f"Error processing {image_id}: {e}")
+        debug_info(debug_messages, image_id, args)
         return image_id, 0, 0, 0, 0, 0, 0, 0, 0, "Y", "Y", image_path, 0
 
 
