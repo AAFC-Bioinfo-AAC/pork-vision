6cf97af755fba9f0e9bd675993912ccd459fc85c
Arun Kommadath <arun.kommadath@agr.gc.ca>
2025-05-30 13:04:53 -0600

    remove private info from slurm directives and add placeholders

--- porkvision.sh
+++ porkvision.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --job-name=porkvision
-#SBATCH --cluster=gpsc8
+#SBATCH --cluster=<cluster name>
 #SBATCH --partition=standard
-#SBATCH --account=aafc_aac
-#SBATCH --time=03:00:00
+#SBATCH --account=<account name>
+#SBATCH --time=<time limit, e.g., 24:00:00 for 24 hours>
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=<number of CPUs> 
+#SBATCH --mem=<amount of memory, e.g., 16G for 16 GB>   
 #SBATCH --output=logs/porkvision_%j.log
 
 conda activate porkvision-1.0.0
