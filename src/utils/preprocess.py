4b8a6168818f8fe2ef95686b3f87a0085498f076
Arun Kommadath <Arun.Kommadath@agr.gc.ca>
2025-10-06 13:01:49 -0600

    Add license information and copyright notice to source files

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,3 +1,20 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2025 His Majesty the King in Right of Canada, as represented by the Minister of Agriculture and Agri-Food, 2025.
+# Pork-vision: pork chop image analysis pipeline
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from skimage.draw import polygon2mask
 from ultralytics.data.utils import polygon2mask
 from utils.imports import *
